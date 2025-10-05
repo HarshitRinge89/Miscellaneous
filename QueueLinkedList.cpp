a50ebba9105469ec8146e3e2dc88d7b46bd5e970
Harshit Ringe <harshit.ringe@gmail.com>
2025-10-05 21:43:18 +0530

    Create QueueLinkedList.cpp

---
+++ QueueLinkedList.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node(){
+        data=0;
+        next=NULL;
+    }
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+class Queue{
+    Node *front,*rear;
+    public:
+    Queue(){
+        front=NULL;
+    }
+    void enqueue(int data){
+        Node *newNode = new Node(data);
+        if(front== NULL){
+            front=rear=newNode;
+            cout<<"Element Inserted"<<endl;
+        }
+        else{
+            rear->next=newNode;
+            rear=newNode;
+            cout<<"Element Inserted"<<endl;
+        }
+    }
+    void display(){
+        Node* temp = front;
+        if(front==NULL){
+            cout<<"Queue is Empty! ";
+        }
+        cout<<"Queue : ";
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+    void dequeue(){
+        Node*prev;
+        if(front==nullptr){
+            cout<<"Queue is Empty";
+        }
+        Node*temp=front;
+        front=front->next;
+        delete temp;
+        cout<<"Element Deleted";
+    }
+};
+int main(){
+    int ch,data,n;
+    Queue q;
+    do{
+        cout<<endl<<"Menu"<<endl;
+        cout<<"1. Enqueue "<<endl;
+        cout<<"2. Dequeue "<<endl;
+        cout<<"3. Display "<<endl;
+        cout<<"4. Exit "<<endl;
+        cout<<"Enter your choice: ";
+        cin>>ch;
+        switch(ch){
+            case 1: cout<<"Enter the value to be inserted: ";cin>>data; q.enqueue(data);break;
+            case 2: q.dequeue();break;
+            case 3: q.display();break;
+            case 4: cout<<"Menu exited!";break;
+            default: cout<<"Invalid Case!"<<endl;
+        }
+    }
+    while(ch!=4);
+}
