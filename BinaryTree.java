14b5d79238e99603a196c35545588b1cdde8f087
Harshit Ringe <harshit.ringe@gmail.com>
2025-11-02 22:46:27 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 class Node{
     int data;Node left,right;
     public Node(int data){
@@ -9,6 +8,7 @@ class Node{
 }
 public class BinaryTree {
     Node root;
+    int level;
     public BinaryTree() {
         root = null;
     }
@@ -20,11 +20,12 @@ public class BinaryTree {
             root = new Node(data);
             return root;
         }
-        if (data < root.data)
+        if (data < root.data){
             root.left = insertRec(root.left, data);
-        else if (data > root.data)
+        }
+        else if (data > root.data){
             root.right = insertRec(root.right, data);
-
+        }
         return root;
     }
     public void show(){
@@ -38,14 +39,59 @@ public class BinaryTree {
             showRec(root.right);
         }
     }
+    private Node deleterec(Node root,int data){
+        if(root==null){
+            System.out.println("Tree is Empty");
+            return root;
+        }
+        if(data<root.data){
+            root.left=deleterec(root.left, data);
+        }
+        else if(data<root.data){
+            root.right=deleterec(root.right, data);
+        }
+        else{
+            if(root.left==null && root.right==null){
+                root=null;
+                return root;
+            }
+            else if(root.left==null){
+                Node temp = root.right;
+                root=null;
+                return temp;
+            }
+            else if(root.right==null){
+                Node temp = root.left;
+                root=null;
+                return temp;
+            }
+            else {
+                while(root.left!=null){
+                    root=root.left;
+                }
+                Node temp=root;
+                root.data=temp.data;
+                root.right=deleterec(root.right, data);
+            }
+        }
+        return root;
+    }
+    public void del(int data){
+        if(root==null){
+            System.out.println("Tree is Empty!");
+        }
+        root=deleterec(root,data);
+        System.out.println("Element Deleted!");
+    }
     public static void main(String[] args) {
         BinaryTree tree=new BinaryTree();
         int ch,data;
         do{
-            System.out.println("Menu");
+            System.out.println("\nMenu");
             System.out.println("1. Insert ");
             System.out.println("2. Show ");
-            System.out.println("3. Exit ");
+            System.out.println("3. Delete");
+            System.out.println("4. Exit ");
             System.out.println("Enter your choice: ");
             Scanner sc = new Scanner(System.in);
             ch= sc.nextInt();
@@ -56,11 +102,16 @@ public class BinaryTree {
                     tree.insert(data);
                     break;
                 case 2: tree.show();break;
+                case 3: 
+                    System.out.println("Enter the Value to be Deleted: ");
+                    data=sc.nextInt();
+                    tree.insert(data);
+                    tree.del(data);
                 case 4: System.out.println("Menu Exited");break;
                 default: System.out.println("Invalid Choice ");
                 sc.close();
             }
         }
-        while(ch!=3);
+        while(ch!=4);
     }
 }
