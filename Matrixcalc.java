66387190a5399049aeb4d07d20f3e3168f536530
Harshit Ringe <harshit.ringe@gmail.com>
2025-11-11 22:38:17 +0530

    Update Matrixcalc.java

--- Matrixcalc.java
+++ Matrixcalc.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 public class Matrix {
     static Scanner sc = new Scanner(System.in);
     static int n,ch;
+    public static int choice(){
+        System.out.println("Select Matrix: ");
+        System.out.println("1. Matrix A");
+        System.out.println("2. Matrix B");
+        System.out.println("Enter your choice: ");
+        int choice=sc.nextInt();
+        return choice;
+    }
     public static void insert(int MatA[][],int MatB[][]){
         int i,j;
         for(i=0;i<n;i++){
@@ -18,12 +26,8 @@ public class Matrix {
         }
     }
     public static void display(int MatA[][],int MatB[][]){
-        System.out.println("Select Matrix to be printed: ");
-        System.out.println("1. Matrix A");
-        System.out.println("2. Matrix B");
-        System.out.println("Enter your choice: ");
-        int dischoice=sc.nextInt();
-        if(dischoice==1){
+        int choice=choice();
+        if(choice==1){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
                     System.out.print(MatA[i][j] + " ");
@@ -31,7 +35,7 @@ public class Matrix {
                 System.out.println();
             }
         }
-        else if(dischoice==2){
+        else if(choice==2){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
                     System.out.print(MatB[i][j] + " ");
@@ -52,27 +56,26 @@ public class Matrix {
         }
     }
     public static void multiplication(int MatA[][],int MatB[][]){
-        int MatC[][]= new int[n][n];
+        int MatC[][]=new int[n][n];
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                System.out.print(MatC[i][j]+=MatA[i][j]*MatB[i][j] + " ");
+                for(int k=0;k<n;k++){
+                    MatC[i][j]+=MatA[i][j]*MatB[i][j];
+                }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                System.out.print(MatC[i][j] + " ");
+                System.out.print(MatC[i][j]+ " ");
             }
             System.out.println();
         }
     }
     public static void determinant(int MatA[][],int MatB[][]){
-        int determinant=0,achoice=0;
-        System.out.println("Select Matrix for Determinant: ");
-        System.out.println("1. Matrix A");
-        System.out.println("2. Matrix B");
-        System.out.println("Enter your choice: ");
-        achoice=sc.nextInt();
-        if(achoice==1){
+        int determinant=0;
+        int choice=choice();
+        if(choice==1){
             if(n==2){
                 determinant=((MatA[0][0]*MatA[1][1])-(MatA[1][2]*MatA[2][1]));
             }
@@ -83,7 +86,7 @@ public class Matrix {
             }
             else{System.out.println("Not supported for order beyond 3!");}
         }
-        else if(achoice==2){
+        else if(choice==2){
             if(n==2){
                 determinant=((MatB[0][0]*MatB[1][1])-(MatB[1][2]*MatB[2][1]));
             }
@@ -101,13 +104,8 @@ public class Matrix {
         System.out.print(determinant);
     }
     public static void minor(int MatA[][],int MatB[][]){
-        int bchoice=0;
-        System.out.println("Select Matrix for Minor: ");
-        System.out.println("1. Matrix A");
-        System.out.println("2. Matrix B");
-        System.out.println("Enter your choice: ");
-        bchoice=sc.nextInt();
-        if(bchoice==1){
+        int choice=choice();
+        if(choice==1){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
                     if((i+j)%2==0){
@@ -118,7 +116,7 @@ public class Matrix {
                 System.out.println();
             }
         }
-        else if(bchoice==2){
+        else if(choice==2){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
                     if((i+j)%2==0){
@@ -134,13 +132,8 @@ public class Matrix {
         }
     }
     public static void cofactor(int MatA[][],int MatB[][],int MatC[][]){
-        int dchoice=0;
-        System.out.println("Select Matrix for Cofactor: ");
-        System.out.println("1. Matrix A");
-        System.out.println("2. Matrix B");
-        System.out.println("Enter your choice: ");
-        dchoice=sc.nextInt();
-        if(dchoice==1){
+        int choice=choice();
+        if(choice==1){
             if(n==2){
                 for(int i=0;i<n;i++){
                     for(int j=0;j<n;j++){
@@ -177,7 +170,7 @@ public class Matrix {
                 System.out.println("Not supported for order beyond 3!");
             }
         }
-        else if(dchoice==2){
+        else if(choice==2){
             if(n==2){
                 for(int i=0;i<n;i++){
                     for(int j=0;j<n;j++){
@@ -219,12 +212,9 @@ public class Matrix {
         }
     }
     public static void transpose(int MatA[][],int MatB[][],int MatC[][]){
-        int echoice=0,temp,temp1,temp2;
-        System.out.println("Select Matrix for Transpose: ");
-        System.out.println("1. Matrix A");
-        System.out.println("2. Matrix B");
-        System.out.println("Enter your choice: ");
-        if(echoice==1){
+        int temp,temp1,temp2;
+        int choice=choice();
+        if(choice==1){
             if(n==2){
                 temp=MatA[1][0];
                 MatA[1][0]=MatA[0][1];
@@ -248,7 +238,7 @@ public class Matrix {
                 System.out.println();
             }
         }
-        else if(echoice==2){
+        else if(choice==2){
             if(n==2){
                 temp=MatB[1][0];
                 MatB[1][0]=MatB[0][1];
@@ -258,12 +248,12 @@ public class Matrix {
                 temp=MatB[1][0];
                 MatB[1][0]=MatB[0][1];
                 MatB[0][1]=temp;
-                temp=MatB[2][0];
+                temp1=MatB[2][0];
                 MatB[2][0]=MatB[0][2];
-                MatB[0][2]=temp;
-                temp=MatB[2][1];
+                MatB[0][2]=temp1;
+                temp2=MatB[2][1];
                 MatB[2][1]=MatB[1][2];
-                MatB[1][2]=temp;
+                MatB[1][2]=temp2;
             }
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
@@ -277,13 +267,9 @@ public class Matrix {
         }
     }
     public static void adjoint(int MatA[][],int MatB[][],int MatC[][]){
-        int echoice=0,temp;
-        System.out.println("Select Matrix for Cofactor: ");
-        System.out.println("1. Matrix A");
-        System.out.println("2. Matrix B");
-        System.out.println("Enter your choice: ");
-        echoice=sc.nextInt();
-        if(echoice==1){
+        int temp,temp1,temp2;
+        int choice=choice();
+        if(choice==1){
             if(n==2){
                 for(int i=0;i<n;i++){
                     for(int j=0;j<n;j++){
@@ -314,15 +300,15 @@ public class Matrix {
                         }
                     }
                 }
-                temp=MatA[1][0];
-                MatA[1][0]=MatA[0][1];
-                MatA[0][1]=temp;
-                temp=MatA[2][0];
-                MatA[2][0]=MatA[0][2];
-                MatA[0][2]=temp;
-                temp=MatA[2][1];
-                MatA[2][1]=MatA[1][2];
-                MatA[1][2]=temp;
+            temp=MatC[1][0];
+            MatC[1][0]=MatC[0][1];
+            MatC[0][1]=temp;
+            temp1=MatC[2][0];
+            MatC[2][0]=MatC[0][2];
+            MatC[0][2]=temp1;
+            temp2=MatC[2][1];
+            MatC[2][1]=MatC[1][2];
+            MatC[1][2]=temp2;
             }
             else{
                 System.out.println("Not supported for order beyond 3!");
@@ -334,7 +320,7 @@ public class Matrix {
                 System.out.println();
             }
         }
-        else if(echoice==2){
+        else if(choice==2){
             if(n==2){
                 for(int i=0;i<n;i++){
                     for(int j=0;j<n;j++){
@@ -365,15 +351,15 @@ public class Matrix {
                         }
                     }
                 }
-                temp=MatB[1][0];
-                MatB[1][0]=MatB[0][1];
-                MatB[0][1]=temp;
-                temp=MatB[2][0];
-                MatB[2][0]=MatB[0][2];
-                MatB[0][2]=temp;
-                temp=MatB[2][1];
-                MatB[2][1]=MatB[1][2];
-                MatB[1][2]=temp;
+            temp=MatC[1][0];
+            MatC[1][0]=MatC[0][1];
+            MatC[0][1]=temp;
+            temp1=MatC[2][0];
+            MatC[2][0]=MatC[0][2];
+            MatC[0][2]=temp1;
+            temp2=MatC[2][1];
+            MatC[2][1]=MatC[1][2];
+            MatC[1][2]=temp2;
             }
             else{
                 System.out.println("Not supported for order beyond 3!");
@@ -389,8 +375,131 @@ public class Matrix {
             System.out.println("Error");
         }
     }
+    public static int det(int MatA[][],int n){
+        int result=0;
+        if(n==2){
+            result=((MatA[0][0]*MatA[1][1])-(MatA[1][2]*MatA[2][1]));
+        }
+        else if(n==3){
+            result=MatA[0][0]*((MatA[1][1]*MatA[2][2])-(MatA[2][1]*MatA[1][2]));
+            result+=MatA[0][1]*((MatA[1][0]*MatA[2][2])-(MatA[2][0]*MatA[1][2]));
+            result+=MatA[0][2]*((MatA[1][0]*MatA[2][1])-(MatA[2][0]*MatA[1][1]));
+        }
+        return result;
+    }
     public static void inverse(int MatA[][],int MatB[][],int MatC[][]){
-        
+        int temp,temp1,temp2,det;
+        float res=0;
+        int choice=choice();
+        if(choice==1){
+            if(n==2){
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        MatC[i][j]=(MatA[0][0]*MatA[1][1])-(MatA[0][1]*MatA[1][0]);
+                        if((i+j)%2==0){
+                            MatC[i][j]*=-1;
+                        }
+                    }
+                }
+                temp=MatA[1][0];
+                MatA[1][0]=MatA[0][1];
+                MatA[0][1]=temp;
+            }
+            else if(n==3){
+                MatC[0][0]=(MatA[1][1]*MatA[2][2])-(MatA[1][2]*MatA[2][1]);
+                MatC[0][1]=(MatA[1][0]*MatA[2][2])-(MatA[1][2]*MatA[2][0]);
+                MatC[0][2]=(MatA[1][1]*MatA[2][0])-(MatA[1][0]*MatA[2][1]);
+                MatC[1][0]=(MatA[0][1]*MatA[2][2])-(MatA[0][2]*MatA[2][1]);
+                MatC[1][1]=(MatA[0][0]*MatA[2][2])-(MatA[0][2]*MatA[2][0]);
+                MatC[1][2]=(MatA[0][0]*MatA[2][1])-(MatA[2][0]*MatA[0][1]);
+                MatC[2][0]=(MatA[0][1]*MatA[1][2])-(MatA[1][1]*MatA[0][2]);
+                MatC[2][1]=(MatA[0][0]*MatA[1][2])-(MatA[1][0]*MatA[0][2]);
+                MatC[2][2]=(MatA[0][0]*MatA[1][1])-(MatA[1][0]*MatA[0][1]);
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        if((i+j)%2==0){
+                            MatC[i][j]*=-1;
+                        }
+                    }
+                }
+            }
+            else{
+                System.out.println("Not supported for order beyond 3!");
+            }
+            temp=MatC[1][0];
+            MatC[1][0]=MatC[0][1];
+            MatC[0][1]=temp;
+            temp1=MatC[2][0];
+            MatC[2][0]=MatC[0][2];
+            MatC[0][2]=temp1;
+            temp2=MatC[2][1];
+            MatC[2][1]=MatC[1][2];
+            MatC[1][2]=temp2;
+            det=det(MatA, n);
+            res=(int)det;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    System.out.print(res*MatC[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+        else if(choice==2){
+            if(n==2){
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        MatC[i][j]=(MatB[0][0]*MatB[1][1])-(MatB[0][1]*MatB[1][0]);
+                        if((i+j)%2==0){
+                            MatC[i][j]*=-1;
+                        }
+                    }
+                }
+                temp=MatB[1][0];
+                MatB[1][0]=MatB[0][1];
+                MatB[0][1]=temp;
+            }
+            else if(n==3){
+                MatC[0][0]=(MatB[1][1]*MatB[2][2])-(MatB[1][2]*MatB[2][1]);
+                MatC[0][1]=(MatB[1][0]*MatB[2][2])-(MatB[1][2]*MatB[2][0]);
+                MatC[0][2]=(MatB[1][1]*MatB[2][0])-(MatA[1][0]*MatB[2][1]);
+                MatC[1][0]=(MatB[0][1]*MatB[2][2])-(MatA[0][2]*MatB[2][1]);
+                MatC[1][1]=(MatB[0][0]*MatB[2][2])-(MatA[0][2]*MatB[2][0]);
+                MatC[1][2]=(MatB[0][0]*MatB[2][1])-(MatA[2][0]*MatB[0][1]);
+                MatC[2][0]=(MatB[0][1]*MatB[1][2])-(MatA[1][1]*MatB[0][2]);
+                MatC[2][1]=(MatB[0][0]*MatB[1][2])-(MatA[1][0]*MatB[0][2]);
+                MatC[2][2]=(MatB[0][0]*MatB[1][1])-(MatA[1][0]*MatB[0][1]);
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        if((i+j)%2==0){
+                            MatC[i][j]*=-1;
+                        }
+                    }
+                }
+            }
+            else{
+                System.out.println("Not supported for order beyond 3!");
+            }
+            temp=MatC[1][0];
+            MatC[1][0]=MatC[0][1];
+            MatC[0][1]=temp;
+            temp1=MatC[2][0];
+            MatC[2][0]=MatC[0][2];
+            MatC[0][2]=temp1;
+            temp2=MatC[2][1];
+            MatC[2][1]=MatC[1][2];
+            MatC[1][2]=temp2;
+            det=det(MatB,n);
+            res=(int)det;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    System.out.print(res*MatB[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+        else{
+            System.out.println("Error");
+        }
     }
     public static void main(String args[]){
         System.out.println("Enter the order of Matrices: ");
@@ -407,7 +516,7 @@ public class Matrix {
         System.out.println("7. Cofactor ");
         System.out.println("8. Transpose ");
         System.out.println("9. Adjoint ");
-        // System.out.println("10. Inverse ");
+        System.out.println("10. Inverse ");
         System.out.println("11. Exit Menu ");
         System.out.println("Enter your choice: ");
         ch=sc.nextInt();
